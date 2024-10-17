fb1f437f82a4f8d7bae694db3c6dcb355ce25c0e
mattsteen14 <mattsteen14@me.com>
2024-10-17 15:33:59 +0100

    weightedRating

--- src/api/igdbApiSlice.js
+++ src/api/igdbApiSlice.js
@@ -1,5 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
+const C = 8.0;
+const m = 50;
+
+const calculateWeightedRating = game => {
+    const R = game.rating || 0;
+    const v = game.rating_count || 0;
+    return ((R * v) + (C * m)) / (v + m);
+}
+
 export const igdbApi = createApi({
     reducerPath: 'igdbApi',
     baseQuery: fetchBaseQuery({ baseUrl: 'https://api.igdb.com/v4/' }),
@@ -14,8 +23,16 @@ export const igdbApi = createApi({
             query: () => ({
                 url: 'games',
                 method: 'POST',
-                body: 'fields name, cover, first_release_date, platforms, genres, summary, websites, rating; limit 100; where category = 0; order rating desc;'
-            })
+                body: 'fields name, cover, first_release_date, platforms, genres, summary, websites, rating, rating_count, involved_companies, screenshots, url; limit 100; where category = 0; order rating desc;'
+            }),
+            transformResponse: (response) => {
+                return response.map(game => {
+                    return {
+                        ...game,
+                        weightedRating: calculateWeightedRating(game),
+                    }
+                }).sort((a, b) => b.weightedRating - a.weightedRating);
+            },
         }),
     }),
 });
