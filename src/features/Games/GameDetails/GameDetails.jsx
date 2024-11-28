e5de7bcf3578f762548103a7b52631420ac1d7d9
mattsteen14 <mattsteen14@me.com>
2024-11-28 11:15:22 +0000

    GameDetails page renders

--- src/features/Games/GameDetails/GameDetails.jsx
+++ src/features/Games/GameDetails/GameDetails.jsx
@@ -15,93 +15,91 @@ export const GameDetails = () => {
     const dispatch = useDispatch();
     const game = useSelector(selectCurrentGame);
     // const similarGames = useSelector((state) => selectSimilarGamesById(state, id));
+
     useEffect(() => {
-        if(!game) {
-            dispatch(fetchGamesByIdThunk(id));
-        }
-    }, [id, dispatch, game]);
-    const {
-        handleYearClick,
-        handlePlatformClick,
-        handleGenreClick
-    } = useFilterHandlers();
+        dispatch(fetchGamesByIdThunk(id));
+    }, [id, dispatch]);
+
+    const { handleYearClick, handlePlatformClick, handleGenreClick } = useFilterHandlers();
+
+    if (!game) {
+        return <div>Loading game...</div>;
+    }
+
     return (
         <div>
-            {game ? (
-                <div className='game-details'>
-                    <section className="game-header">
-                        <span className="title">{game.name}</span>
-                        <a 
+            <div className="game-details">
+                <section className="game-header">
+                    <span className="title">{game.name || "No title available"}</span>
+                    <a
                         className="year"
                         onClick={(e) => handleYearClick(e, game.released)}
-                        >
-                            {game.released}
-                            </a>
-                        {game.developers.length > 0 && (
-                            <span
-                                className="dev"
-                            >
-                                {game.developers[0]}
-                            </span>
-                        )}
-                    </section>
-                    <section className="game-banner">
-                        <img
-                            src={game.background_image}
-                            alt={game.name}
-                            className='game-box-art'
-                        />
+                    >
+                        {game.released ? new Date(game.released).toDateString() : "Unknown release date"}
+                    </a>
+                    {game.developers?.length > 0 && (
+                        <span className="dev">{game.developers[0].name || "Unknown Developer"}</span>
+                    )}
+                </section>
+
+                <section className="game-banner">
+                    <img
+                        src={game.background_image || "fallback_image_url"}
+                        alt={game.name || "Game image"}
+                        className="game-box-art"
+                    />
+                    {game.background_image_additional && (
                         <img
                             src={game.background_image_additional}
                             alt="screenshot"
-                            className='game-screenshot'
+                            className="game-screenshot"
                         />
-                        <aside className="igdb-rating">
-                            <span className="rating">{game.metacritic}</span>
-                            {/* <a href={game.url} target="_blank" rel="noreferrer" className="igdb-link">
-                                <SiIgdb className="igdb-logo" />
-                            </a>
-                            <p className="igdb-note">See IGDB for more info and to rate this game.</p> */}
-                        </aside>
-                    </section>
-                    <section className="game-info-container">
-                        <article className="game-info">
-                            <h4>Genre: </h4>
-                            <a
-                                onClick={(e) => handleGenreClick(e, game.genres)}
-                                href="#"
-                            >
-                                {game.genres.join(", ")}
-                                </a>
-                            <br />
-                            <h4>Platforms: </h4>
-                                <a 
-                                onClick={(e) => handlePlatformClick(e, game.platforms)}
-                                href="#"
-                                >
-                                    {game.platforms}
-                                </a>
+                    )}
+                    <aside className="igdb-rating">
+                        <span className="rating">{game.metacritic || "N/A"}</span>
+                        {/* IGDB Link (commented out) */}
+                    </aside>
+                </section>
+
+                <section className="game-info-container">
+                    <article className="game-info">
+                        <h4>Genre:</h4>
+                        <a onClick={(e) => handleGenreClick(e, game.genres)} href="#">
+                            {game.genres?.map((genre) => genre.name).join(", ") || "No genres available"}
+                        </a>
+                        <br />
+                        <h4>Platforms:</h4>
+                        <a onClick={(e) => handlePlatformClick(e, game.platforms)} href="#">
+                            {game.platforms
+                                ?.map((platformObj) => platformObj.platform.name)
+                                .join(", ") || "No platforms available"}
+                        </a>
+                    </article>
+
+                    <aside className="game-aside">
+                        <h4>Links:</h4>
+                        <article className="links">
+                            <BiLinkExternal />
+                            <HiOutlineDocument />
+                            <FaWikipediaW />
+                            <FaTwitch />
                         </article>
-                        <aside className="game-aside">
-                            <h4>Links:</h4>
-                            <article className="links">
-                                <BiLinkExternal />
-                                <HiOutlineDocument />
-                                <FaWikipediaW />
-                                <FaTwitch />
-                            </article>
-                            <h4>Age Ratings:</h4>
-                            <span className="age-ratings">
-                                {game.esrb_rating.name}
-                                </span>
-                        </aside>
-                    </section>
-                    <section className="game-description">
-                        <h4>Description: </h4>
-                        <p>{game.description_raw}</p>
-                    </section>
-                    <footer>
-                        {/* <h3>Similar Games:</h3>
+                        <h4>Age Ratings:</h4>
+                        {game.esrb_rating ? (
+                            <span className="age-ratings">{game.esrb_rating.name}</span>
+                        ) : (
+                            <span className="age-ratings">Not Rated</span>
+                        )}
+                    </aside>
+                </section>
+
+                <section className="game-description">
+                    <h4>Description:</h4>
+                    <p>{game.description_raw || "No description available"}</p>
+                </section>
+
+                <footer>
+                    {/* <h3>Similar Games:</h3>
                         <div
                         className="similar-games-container"
                         >
@@ -119,11 +117,8 @@ export const GameDetails = () => {
                                 </Link>
                         ))}
                         </div> */}
-                    </footer>
-                </div>
-            ) : (
-                <p>Game not found</p>
-            )}
+                </footer>
+            </div>
         </div>
     );
 };
