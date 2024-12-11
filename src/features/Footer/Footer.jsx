7c98e911126c2725bdaaa07f734ce15ac3f6d94e
mattsteen14 <mattsteen14@me.com>
2024-12-11 13:57:02 +0000

    Footer - back to top button

--- src/features/Footer/Footer.jsx
+++ src/features/Footer/Footer.jsx
@@ -1,27 +1,35 @@
 import './Footer.css'
+import { BsCaretUpSquareFill } from "react-icons/bs";
 
 export const Footer = () => {
     return (
         <footer>
             <span>
-            Developed by  
-                <a 
-                className="github-logo"
-                href="https://mattsteen14.github.io/portfolio/" 
-                target="_blank" 
-                rel="noreferrer"
-                > 
-                Matt Steen-Brookes
+                Developed by
+                <a
+                    className="github-logo"
+                    href="https://mattsteen14.github.io/portfolio/"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    Matt Steen-Brookes
                 </a>
                 . Powered by
-                <a 
-                className="rawg-logo"
-                href="https://www.rawg.io/" 
-                target="_blank" 
-                rel="noreferrer"
-                > 
-                RAWG
-                </a></span>
+                <a
+                    className="rawg-logo"
+                    href="https://www.rawg.io/"
+                    target="_blank"
+                    rel="noreferrer"
+                >
+                    RAWG
+                </a>
+            </span>
+            <a>
+                <BsCaretUpSquareFill
+                    className="top-arrow"
+                    onClick={() => window.scrollTo(0, 0)}
+                />
+            </a>
         </footer>
     )
 }
