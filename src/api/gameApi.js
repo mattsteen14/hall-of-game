de5a41d11d9eb49d48c13875e76d01062303df05
mattsteen14 <mattsteen14@me.com>
2025-01-17 12:16:22 +0000

    search not ordered by metacritic score to improve results

--- src/api/gameApi.js
+++ src/api/gameApi.js
@@ -8,20 +8,24 @@ const handleError = (error) => {
 export const fetchGames = async (page, filters) => {
     try {
         const { platform, genre, year, search, parentPlatform } = filters;
-        const response = await api.get(`/games`, {
-            params: {
-                key: import.meta.env.VITE_API_KEY,
-                ordering: '-metacritic',
-                page,
-                page_size: 40,
-                // exclude_additions: 'true',
-                platforms: platform || undefined, // Include only if provided
-                genres: genre || undefined,
-                dates: year ? `${year}-01-01,${year}-12-31` : undefined,
-                search: search || undefined,
-                parent_platforms: parentPlatform || undefined
-            },
-        });
+
+        const params = {
+            key: import.meta.env.VITE_API_KEY,
+            page,
+            page_size: 40,
+            platforms: platform || undefined,
+            genres: genre || undefined,
+            dates: year ? `${year}-01-01,${year}-12-31` : undefined,
+            search: search || undefined,
+            parent_platforms: parentPlatform || undefined,
+        };
+
+        // Apply ordering only if there's no search term
+        if (!search) {
+            params.ordering = '-metacritic';
+        }
+
+        const response = await api.get(`/games`, { params });
         console.log('Games fetched successfully:', response.data);
         return response.data;
     } catch (error) {
